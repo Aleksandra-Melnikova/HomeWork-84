a7d9e9c298afa26c04652b910f173320fe832f77
Aleksandra Melnikova <Aleksa1992.14.11@mail.ru>
2025-01-10 11:48:02 +0600

    Create models User and Tack ahd create route users

--- Api/index.ts
+++ Api/index.ts
@@ -1,29 +1,29 @@
-// import express from "express";
-// import usersRouter from './routers/users';
-// import config from "./config";
-//
-// import * as mongoose from "mongoose";
-//
-//
-//
-// const app = express();
-// const port = 8000;
-//
-// // app.use(cors());
-// app.use(express.json());
-//
-// app.use('/users', usersRouter);
-//
+import express from "express";
+import usersRouter from './routers/users';
+import config from "./config";
+
+import * as mongoose from "mongoose";
+
+
+
+const app = express();
+const port = 8000;
+
+// app.use(cors());
+app.use(express.json());
+
+app.use('/users', usersRouter);
+
 // app.use('/tasks', tasksRouter);
-//
-// const run = async () => {
-// await mongoose.connect(config.db);
-//
-//     app.listen(port, () => {
-//         console.log(`Server started on port http://localhost:${port}`);
-//     });
-// };
-//
-// run().catch(err => console.log(err));
+
+const run = async () => {
+await mongoose.connect(config.db);
+
+    app.listen(port, () => {
+        console.log(`Server started on port http://localhost:${port}`);
+    });
+};
+
+run().catch(err => console.log(err));
 
 
---
+++ Api/models/Task.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const TaskSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: [true,'user is required'],
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        default: null,
+    },
+    status: {
+        type: String,
+        enum : ['new','in_progress','complete'],
+        default: 'new',
+    }
+});
+
+const Task = mongoose.model("Task", TaskSchema);
+
+export default Task;
